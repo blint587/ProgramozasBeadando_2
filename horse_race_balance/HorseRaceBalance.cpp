368145f4c86ac5edd4eb13be38a397ef99c5dd2c
balint <blint587@gmail.com>
2016-03-14 14:24:40 +0100

    simple push

--- horse_race_balance/HorseRaceBalance.cpp
+++ horse_race_balance/HorseRaceBalance.cpp
@@ -31,14 +31,14 @@ bool HorseRaceBalance::won_affter_loisng_(int const k) const {
 
     while ( !is_it && p_current < this->_incomes.end()){
 
-        if((* p_current  <= 0. && this->_zero_is_a_loss) || * p_current < 0. && !this->_zero_is_a_loss){
+        if((* p_current  <= 0. && this->_zero_is_a_loss) || (* p_current < 0. && !this->_zero_is_a_loss)){
             ++count_of_loss;
         }
         else{
             count_of_loss = 0;
         }
+        is_it = (k <= count_of_loss && ((* p_current  > 0. && this->_zero_is_a_loss) || (* p_current >= 0. && !this->_zero_is_a_loss)));
         p_current++;
-        is_it = (k == count_of_loss);
     }
 
 
--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main(){
 
-    vector<double> s = {0.,0.,0.,1.,2.,3.,4.};
+    vector<double> s = {0.,0.,0.,0.,0.,0.,0.};
 
-    HorseRaceBalance hr = HorseRaceBalance(s, true);
+    HorseRaceBalance hr = HorseRaceBalance(s, false);
 
     cout << hr.won_affter_loisng_(2) << endl;
     return 0;
